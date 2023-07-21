f56197fe6597b27ab51790dac1de61660df21546
Adam Zhu <lei.zhu@live.cn>
2023-07-21 15:15:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import { Bot } from 'grammy'
 
 const bot = new Bot("6069145648:AAEx2eSIHDY_za9zXM1TqlsavQjpcubzGH4") // <-- Set your token in the vercel environment variable
 
-bot.api.setWebhook("https://my-first-telegram-b677xvlgy-adam-zhu-1991.vercel.app/api/bot")
+bot.api.setWebhook("https://my-first-telegram-bot.vercel.app/api/bot")
